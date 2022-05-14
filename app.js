0b495ca5e019bcb6ea12648f1eaaeafc6a1a89bc
wn5865 <wn2262@gmail.com>
2022-05-14 15:33:33 +0900

    Enable CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,10 +25,13 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+// 1) GLOGAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
 // Compress responses
 app.use(compression());
 
-// 1) GLOGAL MIDDLEWARES
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
