ba02aa276926a8accee76c3c22dec3eefc8e2e72
wn5865 <wn2262@gmail.com>
2022-05-14 21:41:58 +0900

    Correct errors in stripe webhook implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -41,6 +41,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
+  console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
