3df2fde0d8f4bfa60c18ef2199615b29055ec7f0
wn5865 <wn2262@gmail.com>
2022-05-26 11:17:21 +0900

    Correct a bug in saving dev-data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,18 +4,20 @@ const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
 const Review = require('../../models/reviewModel');
 const User = require('../../models/userModel');
+const Booking = require('../../models/bookingModel');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
+const DB = process.env.DATABASE_URL.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
 // Connect to DB
-mongoose.connect(DB).then(() => {
-  console.log('DB connection sucessful!');
-});
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection sucessful!'))
+  .catch(() => console.log('DB connection failed!'));
 
 // Read JSON file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
@@ -27,6 +29,7 @@ const reviews = JSON.parse(
 // Import data into DB
 const importData = async () => {
   try {
+    process.env.NODE_ENV = 'import';
     await Tour.create(tours);
     await User.create(users, { validateBeforeSave: false });
     await Review.create(reviews);
@@ -43,6 +46,7 @@ const deleteData = async () => {
     await Tour.deleteMany();
     await User.deleteMany();
     await Review.deleteMany();
+    await Booking.deleteMany();
     console.log('data successfully deleted');
   } catch (err) {
     console.log(err);
