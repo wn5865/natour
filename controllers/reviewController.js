54847d174b0d3c8ac49c3750f4395f6861a78c5a
wn5865 <wn2262@gmail.com>
2022-06-04 13:06:56 +0900

    simplify setTourAndUser function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,19 +1,16 @@
 const Review = require('../models/reviewModel');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('../controllers/handlerFactory');
-
-/**
- * Middleware to set user and tour IDs
- */
-exports.setTourUserIds = catchAsync(async (req, res, next) => {
-  // Set user and tour IDs
-  if (!req.body.user) req.body.user = req.user.id;
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  next();
-});
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/userModel');
 
 exports.getAllReviews = factory.getAll(Review);
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
 exports.updateReview = factory.updateOne(Review);
+
+exports.setTourAndUser = (req, res, next) => {
+  req.body.tour ||= req.params.tourId;
+  req.body.user = req.user.id;
+  next();
+};
