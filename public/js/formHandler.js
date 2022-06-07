04b9fbded0d9335c4bc7c96640d424f76cca48b2
wn5865 <wn2262@gmail.com>
2022-06-07 14:36:14 +0900

    Fix a bug

--- public/js/formHandler.js
+++ public/js/formHandler.js
@@ -1,6 +1,6 @@
 export const handleForm = function (event) {
   event.preventDefault();
-  const formData = new FormData(event.target);
+  const formData = new FormData(event.target.closest('.form'));
   const data = Object.fromEntries(formData.entries());
   return data;
 };
