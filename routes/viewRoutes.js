3e70df863decdebbd5aace29d40f5af13f6a71a9
wn5865 <wn2262@gmail.com>
2022-06-08 00:31:48 +0900

    Change some routes and middlewares

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,7 +8,7 @@ router.use(viewsController.alerts);
 router.use(authController.isLoggedIn);
 
 router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', viewsController.getTour);
+router.get('/tour-detail/:slug', viewsController.getTour);
 router.get('/login', viewsController.getLoginForm);
 router.get('/signup', viewsController.getSignupForm);
 
@@ -21,7 +21,10 @@ router.get('/review/:tourId/:dateId', viewsController.getReviewForm);
 
 router.use(authController.restrictTo('admin'));
 
-// router.get('manage-tours', viewsController.manageTours);
+router.get('/manage-tours', viewsController.manageTours);
+router.get('/tour', viewsController.getTourForm);
+router.get('/tour/:tourId', viewsController.getTourForm);
+// router.get('/manage-tours/:tourId', viewsController.getTourForm);
 // router.get('manage-users', viewsController.manageUsers);
 // router.get('manage-reviews', viewsController.manageReviews);
 // router.get('manage-bookings', viewsController.manageBookings);
