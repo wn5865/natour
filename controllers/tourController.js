b8a4a32f38c90609dea096ce8b3af45ec67014b2
wn5865 <wn2262@gmail.com>
2022-06-09 22:50:14 +0900

    Fix a bug in user info update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -53,12 +53,12 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
  * specified year
  */
 exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
-  const year = Number(req.params.year);
+  const year = req.params.year;
   const plan = await Tour.aggregate([
     { $unwind: '$startDates' },
     {
       $match: {
-        startDates: {
+        'startDates.date': {
           $gte: new Date(`${year}-01-01`),
           $lte: new Date(`${year}-12-31`),
         },
@@ -66,7 +66,7 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     },
     {
       $group: {
-        _id: { $month: '$startDates' },
+        _id: { $month: '$startDates.date' },
         numTourStarts: { $sum: 1 },
         tours: { $push: '$name' },
       },
